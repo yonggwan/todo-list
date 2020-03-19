841d5670eaf40aaa6f29d86fa9fb2ca5f4b65503
yonggwan <choiyk1225@gmail.com>
2020-03-19 22:45:58 +0900

    init firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCxRD8YQMrW88g2wz4f6cPo97QFYwIDn2E",
+  authDomain: "todo-f1e63.firebaseapp.com",
+  databaseURL: "https://todo-f1e63.firebaseio.com",
+  projectId: "todo-f1e63",
+  storageBucket: "todo-f1e63.appspot.com",
+  messagingSenderId: "958000585998",
+  appId: "1:958000585998:web:6a08e6798f5b91088ecb5f",
+  measurementId: "G-5K5139DSLJ"
+};
---
+++ src/firebase/index.ts
@@ -0,0 +1,9 @@
+
+import firebase from 'firebase';
+import config from './config';
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
+
+export default firebase;
