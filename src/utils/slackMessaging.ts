dbe633242d44953bd519fc2de88eb5dc6d84fab8
yonggwan <yonggwan@pren.kr>
2020-06-01 11:55:22 +0900

    fix slack webhook url

--- src/utils/slackMessaging.ts
+++ src/utils/slackMessaging.ts
@@ -1,5 +1,5 @@
 // channel: yongs / yongspace / palyground
-const SlackURL = 'https://hooks.slack.com/services/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';
+const SlackURL = 'https://hooks.slack.com/services/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';
 
 export const getNewTodoMessagingBody = (description: string) => ({
   "text": "Someone left new todo!",
